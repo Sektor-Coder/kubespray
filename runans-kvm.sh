444c1fa5270119e835206df300429c32a734445b
Thanos Athanasopoulos <thanos.athanasopoulos@intrasoft-intl.com>
2020-03-27 22:06:04 +0200

    Profilization.

--- runans-kvm.sh
+++ runans-kvm.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-ansible-playbook -v -i inventory/kvmcluster/hosts.ini --private-key=~/.ssh/id_rsa.pub --become -e ansible_ssh_user=root cluster.yml
+ansible-playbook -v -i inventory/local/hosts.ini --private-key=~/.ssh/id_rsa_vm.pub --become -e ansible_ssh_user=root cluster.yml
